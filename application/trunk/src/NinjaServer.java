1c0a40276e1f66883c923cf9fe217a5e8a50806e
jason.pell <jason.pell@9ec14738-e444-0410-803a-9761859931cd>
2008-03-06 02:39:22 +0000

    - added MIT license headers - added debug fields (useclient, userobot)

--- application/trunk/src/NinjaServer.java
+++ application/trunk/src/NinjaServer.java
@@ -1,5 +1,32 @@
+ /******************************************************************************
+* Copyright (C) 2008 Jason Pell. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+******************************************************************************/
+
 
 public class NinjaServer {
+	
+	public static boolean ROBOTON;
+	public static boolean CLIENTON;
+	
 	public NinjaServer()
 	{
 		
@@ -7,11 +34,24 @@ public class NinjaServer {
 	
 	public static void main(String[] args)
 	{
-		RobotInterface robot = new RobotInterface();
-		ClientInterface client = new ClientInterface();
+		RobotInterface robot = null;
+		ClientInterface client = null;
+		ROBOTON = true;
+		CLIENTON = true;
+
+		if (ROBOTON)
+		{
+			robot = new RobotInterface();
+		}
+		
+		if (CLIENTON)
+		{
+			client = new ClientInterface();
+		}
 		//ImageDetection detection = new ImageDetection();
 		//VideoStreaming streaming = new VideoStreaming();
 		
+
 		//robot.run();
 		//client.run();
 		//detection.run();
@@ -20,24 +60,35 @@ public class NinjaServer {
 		while (true)
 		{
 			String command;
-			command = client.receiveCommand();
-			System.out.println(command);
-		
-			robot.sendCommand(command);
 			
-			if (command.contains("x"))
+			if (CLIENTON)
 			{
-				break;
-			}
-			if (robot.receiveStatus())
-			{
-				//client.sendAck();
-				System.out.println("Success!");
-			}
-			else
-			{
-				System.out.println("Failure.");
-				//client.sendNak();
+				command = client.receiveCommand();
+				System.out.println(command);
+		
+				if (ROBOTON)
+				{
+				   robot.sendCommand(command);
+				}
+				
+				if (command.contains("X"))
+				{
+					break;
+				}
+				
+				if (ROBOTON)
+				{
+					if (robot.receiveStatus())
+					{
+						//client.sendAck();
+						System.out.println("Success!");
+					}
+					else
+					{
+						System.out.println("Failure.");
+						//client.sendNak();
+					}
+				}
 			}
 		}
 		
