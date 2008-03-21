fdb795166a1cd8148c7a741bc46b382043d9a6db
parkeada <parkeada@9ec14738-e444-0410-803a-9761859931cd>
2008-03-21 23:08:04 +0000

    Updated keyboard controls

--- client/trunk/src/ActionSet.java
+++ client/trunk/src/ActionSet.java
@@ -5,42 +5,51 @@ class ActionSet extends AbstractAction
 	private String action = "";
 	private EchoClient echo;
 	private JSlider slider;
+	private String temp = "";
+	private NinjaGUI ninja;
 
-    public ActionSet(String pAction, JSlider pSlider) 
+    public ActionSet(String pAction, JSlider pSlider, NinjaGUI pNinja) 
     {
     	  super();
     	  action = pAction;
     	  //echo = new EchoClient("127.0.0.1", 8080);
     	  slider = pSlider;
+    	  ninja = pNinja;
     }
     
     public void actionPerformed(ActionEvent e) 
     {
-     	String speed = "000000";
-		speed = speed + slider.getValue();
-		speed = speed.substring(speed.length() - 3, speed.length());
-		
-		if(action == "forward")
+		if(action != ninja.getCurrentCom())
 		{
-			action = "L+"+speed+"R+"+speed;
-		}
-		else if (action == "backward")
-		{
-			action = "L-"+speed+"R-"+speed;
-		}
-		else if (action == "left")
-		{
-			action = "L+000R+"+speed;
-		}
-		else if (action == "right")
-		{
-			action = "L+"+speed+"R+000";
-		}
-		else if (action == "halt")
-		{
-			action = "L+000R+000";
-		}
-		System.out.println(action);
+		   String speed = "000000";
+			speed = speed + slider.getValue();
+			speed = speed.substring(speed.length() - 3, speed.length());
+			String tcpCommand = "";
+			
+			if(action == "forward")
+			{
+				tcpCommand = "L+"+speed+"R+"+speed;
+			}
+			else if (action == "backward")
+			{
+				tcpCommand = "L-"+speed+"R-"+speed;
+			}
+			else if (action == "left")
+			{
+				tcpCommand = "L+000R+"+speed;
+			}
+			else if (action == "right")
+			{
+				tcpCommand = "L+"+speed+"R+000";
+			}
+			else if (action == "halt")
+			{
+				tcpCommand = "L+000R+000";
+			}
+			ninja.setCurrentCom(action);
+			System.out.println(action);
+			System.out.println(tcpCommand);
 		//echo.sendString(action);
+		}
     }
 }
