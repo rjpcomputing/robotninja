e64ced9b492abac3f4516a0efb8dba40537eedba
parkeada <parkeada@9ec14738-e444-0410-803a-9761859931cd>
2008-03-30 05:56:32 +0000

    Added Features: - 'O' now opens the claw - 'P' now closes the claw - Optional command line parameters can be entered in the form <server> <port> <video port>. - Connect and Disconnect buttons will toggle to prevent user confusion or error. - Arrangement of various components. - Score area added.

--- client/trunk/src/NinjaGUI.java
+++ client/trunk/src/NinjaGUI.java
@@ -20,82 +20,58 @@ import javax.media.bean.playerbean.*;
 
 public class NinjaGUI extends JFrame implements ActionListener
 {
+	private ActionSet forward, backward, left, right, halt, open, close;
+	private Container content;
+	private int serverPort;
+	private GetVideo media;
 	private JButton btnConnect, btnDisconnect;
-	private JTextField jtxServer, jtxPort, jtxVidPort;
+	private JLabel lblServer, lblPort, lblVidPort, lblPower, lblScore;
+	private JPanel video;
 	private JSlider slider;
+	private JTextArea txtInstructions;
+	private JTextField jtxServer, jtxPort, jtxVidPort, jtxTeamOne, jtxTeamTwo;
+	private MediaPlayer player;
 	private String serverName;
 	private Socket sktToServer;
-	private int serverPort;
-	private Container content;
 	public String command;
 	public EchoClient client;
-	private MediaPlayer player;
-	private ActionSet forward, backward, left, right, halt, open, close;
-	private JPanel video;
-	private JTextArea txtInstructions;
-	private JLabel lblServer, lblPort, lblVidPort;
 	
 	// Constructor
-	NinjaGUI() 
+	NinjaGUI(String[] pArgs) 
 	{
 		super("NinjaBot");
-		init();
+		init(pArgs);
 	}
 	
 	// Main initialization method
-	protected void init() 
+	protected void init(String[] pArgs) 
 	{   
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		setSize(700,750);
+		setSize(700,700);
 		setResizable(false);
 		
 		addWindowListener(new WindowAdapter() 
     	{
     		public void windowClosing(WindowEvent e) 
     		{
-				//player.stop();
+				if(client != null)
+				{
+					client.sendString("X.........");
+				}
+				if(player != null)
+				{
+					player.stop();
+				}
     			System.exit(0);
     		}
 		});
 		
-		btnDisconnect = new JButton("Disconnect");
-		btnDisconnect.setBounds(525, 80, 100, 25);
-		btnDisconnect.addActionListener(this);
-		btnDisconnect.setActionCommand("X.........");
+		lblPower = new JLabel("<html>P<br/>o<br/>w<br/>e<br/>r<br/></html>");
+		lblPower.setBounds(35,5,10,125);
+		lblPower.setForeground(Color.WHITE);
 		
-		btnConnect = new JButton("Connect");
-		btnConnect.setBounds(525, 30, 100, 25);
-		btnConnect.addActionListener(this);
-		btnConnect.setActionCommand("CONNECTION");
-		
-		lblServer = new JLabel("Server:");
-		lblServer.setBounds(300, 30, 50, 25);
-		lblServer.setForeground(Color.WHITE);
-		jtxServer = new JTextField();
-		jtxServer.setBounds(350, 30, 150, 25);
-		
-		txtInstructions = new JTextArea("W - Forward\nA - Left\nS - Backward\nD - Right\nE - Stop\nUp - Open Claw\nDown - Close Claw");
-		txtInstructions.setBounds(160, 25, 125, 125);
-		txtInstructions.setForeground(Color.WHITE);
-		txtInstructions.setBackground(Color.BLACK);
-		txtInstructions.setEditable(false);
-		
-		lblPort = new JLabel("Port:");
-		lblPort.setBounds(300, 80, 50, 25);
-		lblPort.setForeground(Color.WHITE);
-		jtxPort = new JTextField();
-		jtxPort.setBounds(350, 80, 150, 25);
-		
-		lblVidPort = new JLabel("Video Port:");
-		lblVidPort.setBounds(300, 120, 50, 25);
-		lblVidPort.setForeground(Color.WHITE);
-		jtxVidPort = new JTextField();
-		jtxVidPort.setBounds(350, 120, 150, 25);
-		
-		video = new JPanel();
-    	
-    	slider = new JSlider(JSlider.VERTICAL);
-		slider.setBounds(75, 25, 50, 125);
+		slider = new JSlider(JSlider.VERTICAL);
+		slider.setBounds(50, 5, 50, 125);
 		slider.setMajorTickSpacing(25);
 		slider.setPaintTicks(true);
 		
@@ -110,19 +86,90 @@ public class NinjaGUI extends JFrame implements ActionListener
 		slider.setLabelTable( labelTable );
 		slider.setPaintLabels(true);
 		
+		txtInstructions = new JTextArea("W - Forward\nA - Left\nS - Backward\nD - Right\nE - Stop\nO - Open Claw\nP - Close Claw");
+		txtInstructions.setBounds(137, 5, 100, 125);
+		txtInstructions.setForeground(Color.WHITE);
+		txtInstructions.setBackground(Color.BLACK);
+		txtInstructions.setEditable(false);		
+		
+		lblServer = new JLabel("Server:");
+		lblServer.setBounds(250, 5, 75, 25);
+		lblServer.setForeground(Color.WHITE);
+		
+		lblPort = new JLabel("Port:");
+		lblPort.setBounds(250, 40, 75, 25);
+		lblPort.setForeground(Color.WHITE);
+		
+		lblVidPort = new JLabel("Video Port:");
+		lblVidPort.setBounds(250, 75, 75, 25);
+		lblVidPort.setForeground(Color.WHITE);
+
+		jtxServer = new JTextField();
+		jtxServer.setBounds(325, 5, 150, 25);
+		if(pArgs.length > 0)
+		{
+			jtxServer.setText(pArgs[0]);
+		}
+		
+		jtxPort = new JTextField();
+		jtxPort.setBounds(325, 40, 150, 25);
+		if(pArgs.length > 1)
+		{
+			jtxPort.setText(pArgs[1]);
+		}
+		
+		jtxVidPort = new JTextField();
+		jtxVidPort.setBounds(325, 75, 150, 25);
+		if(pArgs.length > 2)
+		{
+			jtxVidPort.setText(pArgs[2]);
+		}
+		
+		btnConnect = new JButton("Connect");
+		btnConnect.setBounds(500, 5, 150, 25);
+		btnConnect.addActionListener(this);
+		btnConnect.setActionCommand("CONNECTION");
+		
+		btnDisconnect = new JButton("Disconnect");
+		btnDisconnect.setBounds(500, 40, 150, 25);
+		btnDisconnect.addActionListener(this);
+		btnDisconnect.setActionCommand("X.........");	
+		btnDisconnect.setEnabled(false);
+		
+		lblScore = new JLabel("Score");
+		lblScore.setBounds(550, 75, 100, 25);
+		lblScore.setForeground(Color.WHITE);
+		
+		jtxTeamOne = new JTextField("0");
+		jtxTeamOne.setBounds(500, 100, 50, 25);
+		jtxTeamOne.setForeground(Color.RED);
+		jtxTeamOne.setEditable(false);
+		
+		jtxTeamTwo = new JTextField("0");
+		jtxTeamTwo.setBounds(575, 100, 50, 25);
+		jtxTeamTwo.setForeground(Color.BLUE);
+		jtxTeamTwo.setEditable(false);
+
+		video = new JPanel();
+		
 		content = getContentPane();
 		content.setLayout(null);
+		
+		content.add(video);
+		content.add(lblPower);
 		content.add(slider);
-    	content.add(video);
-    	content.add(btnDisconnect);
-		content.add(btnConnect);
-		content.add(jtxServer);
-		content.add(jtxPort);
-		content.add(jtxVidPort);
+		content.add(txtInstructions);
 		content.add(lblServer);
 		content.add(lblPort);
 		content.add(lblVidPort);
-		content.add(txtInstructions);
+		content.add(jtxServer);
+		content.add(jtxPort);
+		content.add(jtxVidPort);
+		content.add(btnConnect);
+		content.add(btnDisconnect);
+		content.add(lblScore);
+		content.add(jtxTeamOne);
+		content.add(jtxTeamTwo);
 		content.setBackground(Color.BLACK);
     	setVisible(true);
 	}
@@ -164,16 +211,16 @@ public class NinjaGUI extends JFrame implements ActionListener
 			video.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("A"),"a_pressed");
 			video.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("D"),"d_pressed");
 			video.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("E"),"e_pressed");
-			video.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("Y"),"up_pressed");
-			video.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("H"),"down_pressed");
+			video.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("O"),"o_pressed");
+			video.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("P"),"p_pressed");
 
 			video.getActionMap().put("w_pressed", forward);
 			video.getActionMap().put("s_pressed", backward);
 			video.getActionMap().put("a_pressed", left);
 			video.getActionMap().put("d_pressed", right);
 			video.getActionMap().put("e_pressed", halt);
-			video.getActionMap().put("up_pressed", open);
-			video.getActionMap().put("down_pressed", close);
+			video.getActionMap().put("o_pressed", open);
+			video.getActionMap().put("p_pressed", close);
 			
 			try
 			{
@@ -184,12 +231,16 @@ public class NinjaGUI extends JFrame implements ActionListener
 				System.out.println(e.toString());
 			}
 			String tempVidPort = jtxVidPort.getText().trim();
-			GetVideo media = new GetVideo(tempServer, tempVidPort);
-	    	MediaPlayer player = media.getGUI();
-			player.setBounds(25, 175, 640, 480);
+			media = new GetVideo(tempServer, tempVidPort);
+	    	player = media.getGUI();
+			player.setBounds(25, 150, 640, 480);
 			player.start();
 			
+			btnDisconnect.setEnabled(true);
+			btnConnect.setEnabled(false);
+			
 			Graphics g = content.getGraphics();
+			content.remove(lblPower);
 			content.remove(slider);
 	    	content.remove(video);
 	    	content.remove(btnDisconnect);
@@ -201,8 +252,13 @@ public class NinjaGUI extends JFrame implements ActionListener
 			content.remove(lblPort);
 			content.remove(lblVidPort);
 			content.remove(txtInstructions);
+			content.remove(lblScore);
+			content.remove(jtxTeamOne);
+			content.remove(jtxTeamTwo);
 			content.setBackground(null);
-			update(g);
+			update(g);
+
+			content.add(lblPower);
 			content.add(slider);
 	    	content.add(video);
 	    	content.add(btnDisconnect);
@@ -214,11 +270,69 @@ public class NinjaGUI extends JFrame implements ActionListener
 			content.add(lblPort);
 			content.add(lblVidPort);
 			content.add(txtInstructions);
+			content.add(lblScore);
+			content.add(jtxTeamOne);
+			content.add(jtxTeamTwo);
 			content.setBackground(Color.BLACK);
 			content.add(player);
 			update(g);
 		}
-		System.out.println(action);
+		//System.out.println(action);
+		if(action == "X.........")
+		{
+			player.stop();
+			media = null;
+			player = null;
+			client = null;
+			forward = null;
+			backward = null;
+			left = null;
+			right = null;
+			halt = null;
+			open = null;
+			close = null;
+			
+			btnDisconnect.setEnabled(false);
+			btnConnect.setEnabled(true);
+			
+			Graphics g = content.getGraphics();
+			content.remove(lblPower);
+			content.remove(slider);
+	    	content.remove(video);
+	    	content.remove(btnDisconnect);
+			content.remove(btnConnect);
+			content.remove(jtxServer);
+			content.remove(jtxPort);
+			content.remove(jtxVidPort);
+			content.remove(lblServer);
+			content.remove(lblPort);
+			content.remove(lblVidPort);
+			content.remove(txtInstructions);
+			content.remove(player);
+			content.remove(lblScore);
+			content.remove(jtxTeamOne);
+			content.remove(jtxTeamTwo);
+			content.setBackground(null);
+			update(g);
+
+			content.add(lblPower);
+			content.add(slider);
+	    	content.add(video);
+	    	content.add(btnDisconnect);
+			content.add(btnConnect);
+			content.add(jtxServer);
+			content.add(jtxPort);
+			content.add(jtxVidPort);
+			content.add(lblServer);
+			content.add(lblPort);
+			content.add(lblVidPort);
+			content.add(txtInstructions);
+			content.add(lblScore);
+			content.add(jtxTeamOne);
+			content.add(jtxTeamTwo);
+			content.setBackground(Color.BLACK);
+			update(g);
+		}
 		client.sendString(action);
 	}
 	
@@ -226,6 +340,6 @@ public class NinjaGUI extends JFrame implements ActionListener
 	public static void main(String[] args)
 	{
 		System.out.println("Go Ninja, Go Ninja, Go!");
-		new NinjaGUI();
+		new NinjaGUI(args);
 	}
 }
